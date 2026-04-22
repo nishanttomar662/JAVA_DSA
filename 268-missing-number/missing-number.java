82f329d874f0bf26eaa4ea826bb6530de997e6fc
Nishant Tomar <nishanttomar948@gmail.com>
2026-04-22 10:22:14 +0530

    Time: 1 ms (29.72%) | Memory: 46.9 MB (95.61%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int [] hasharray=new int[n+1];
+        for(int i=0;i<=n;i++){
+              hasharray[i]=0;
+        }
+        for(int i=0;i<n;i++){
+            hasharray[nums[i]]++;
+        }
+        int res=0;
+        for(int i=0;i<=n;i++){
+            if(hasharray[i]==0)
+            res=i;
+    
+        }
+        return res;
+    }
+}
